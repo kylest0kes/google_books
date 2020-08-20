6d973a8c9269a63542316e4522f214dce5caeb44
kylest0kes <kylestokes1551@gmail.com>
2020-08-19 21:54:31 -0400

    created book card component to hold book info

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import BookCard from './components/BookCard'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Search />
+      <BookCard />
     </div>
   );
 }
