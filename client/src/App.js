70adfc026191c13554f500e5d5773f8706b69385
kylest0kes <kylestokes1551@gmail.com>
2020-08-19 22:00:02 -0400

    added a section title component

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './components/Header';
 import Search from './components/Search';
 import BookCard from './components/BookCard'
+import SectionTitle from './components/SectionTitle';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Search />
+      <SectionTitle sectionTitle={"Results"}/>
       <BookCard />
     </div>
   );
---
+++ client/src/components/SectionTitle/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.css'
+
+export default function SectionTitle(props) {
+    return (
+        <h4 className="title">{props.sectionTitle}</h4>
+    )
+}
