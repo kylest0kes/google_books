d20345bf33cfe3afc0477fe0f9b8f6ddcc45ea95
kylest0kes <kylestokes1551@gmail.com>
2020-08-21 23:16:04 -0400

    set up view button to link to google api

--- client/src/components/BookCard/index.js
+++ client/src/components/BookCard/index.js
@@ -11,7 +11,7 @@ export default function BookCard(props) {
                 <h5>{props.title}</h5>
                 <h6>{props.authors}</h6>
                 <p>{props.description}</p>
-                <button type="button" className="btn btn-primary btn-sm">View</button>
+                <a href={props.link} target="blank"><button type="button" className="btn btn-primary btn-sm">View</button></a>
                 <button type="button" className="btn btn-secondary btn-sm">Save</button>
             </div>
         </div>
