9bbbdf1026bc0eceb14240bfc488112d9bc9ca4f
kylest0kes <kylestokes1551@gmail.com>
2020-08-21 21:44:00 -0400

    created working handleInputChange

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -3,7 +3,7 @@ import GoogleBooksAPI from '../../utils/GoogleBooksAPI';
 
 import './style.css';
 
-export default function Search() {
+export default function Search(props) {
     const formSubmit = e => {
         e.preventDefault();
         console.log("working")
@@ -18,7 +18,7 @@ export default function Search() {
         <div className="search">
             <h4>Search your favorite books here!</h4>
             <div className="input-group input-group-rounded">
-                <input className="input-group-field" type="search"/>
+                <input className="input-group-field" onChange={props.handleInputChange} value={props.searchState.title} name="title" type="search"/>
                 <div className="input-group-button">
                     <input type="submit" onClick={formSubmit} className="button secondary" value="Search"/>
                 </div>
--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import BookCard from '../components/BookCard'
 import SectionTitle from '../components/SectionTitle';
 
 export default function SearchPage() {
+    const [searchState, setSearchState] = useState({
+        title: ""
+    });
+
+    const handleInputChange = e => {
+        const name = e.target.name;
+        let value = e.target.value;
+
+        setSearchState({
+            [name]: value
+        })
+    }
+
     return (
         <div>
             <Header />
-            <Search />
+            <Search 
+            handleInputChange={handleInputChange} 
+            searchState={searchState}
+            />
             <SectionTitle sectionTitle={"Results"}/>
-            <BookCard />
             <BookCard />
         </div>
     )
